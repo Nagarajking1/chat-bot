9fa64e1615321cb77f3b0f17b67d4d7aed62bad6
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-19 12:57:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,6 @@ HEROKU = True
 
 if HEROKU:
     from os import environ
-    ARQ_API_KEY = environ["ARQ_API_KEY"]
-    LANGUAGE = environ["LANGUAGE"]
     ARQ_API_KEY = "GNOZRZ-AVRBJA-RWVIKJ-GZVBXZ-ARQ"
     LANGUAGE = "ta"
     ARQ_API_BASE_URL = "https://thearq.tech"
