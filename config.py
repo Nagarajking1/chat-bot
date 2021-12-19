6ea89f02bf841abf868980767d0a78efd9d491a3
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-19 13:17:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ if HEROKU:
     from os import environ
     ARQ_API_KEY = "GNOZRZ-AVRBJA-RWVIKJ-GZVBXZ-ARQ"
     LANGUAGE = "ta"
-    ARQ_API_BASE_URL = "https://thearq.tech"
+    ARQ_API_BASE_URL = "https://grambuilders.tech"
